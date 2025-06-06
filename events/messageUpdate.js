b9d3438844487543b874774373a1f0e9682a706a
María Ten Rodríguez <materod@gmail.com>
2025-06-06 19:08:38 +0000

    Enhance message update handling to respond when the bot is mentioned; include typing indication and improve response editing logic.

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,5 +1,6 @@
 const { Events } = require('discord.js');
 const logger = require('../logger');
+const { generateBotResponse } = require('../ia/openrouter');
 
 module.exports = {
   name: Events.MessageUpdate,
@@ -7,5 +8,24 @@ module.exports = {
     if (newMessage.author.bot) return;
 
     logger.info(`Message from ${newMessage.author.tag}: ${newMessage.content}`);
+
+    // Responde si el mensaje editado menciona al bot
+    if (newMessage.mentions.users.has(newMessage.client.user.id)) {
+      // Busca la última respuesta del bot al usuario en el canal
+      const fetched = await newMessage.channel.messages.fetch({ limit: 10 });
+      const botReply = fetched.find(m =>
+        m.reference &&
+        m.reference.messageId === newMessage.id &&
+        m.author.id === newMessage.client.user.id
+      );
+
+      await newMessage.channel.sendTyping();
+      const response = await generateBotResponse(newMessage.author.tag, newMessage.content);
+      if (botReply) {
+        await botReply.edit(response);
+      } else {
+        await newMessage.reply(response);
+      }
+    }
   },
 };
